e932b6670da3b549c3a77b330d6b5b7255c370bd
Violet Replicon <vreplicon@gmail.com>
2020-01-25 10:04:29 -0800

    Setting up psql connections

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 const app = require('./app')
-const { PORT } = require('./config')
+const knex = require('knex')
+const {
+    DATABASE_URL,
+    PORT
+} = require('./config')
+
+const db = knex({
+    client: 'pg',
+    connection: DATABASE_URL,
+})
+
+app.set('db', db)
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
