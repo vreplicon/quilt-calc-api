9a701072710f22ef4187f262b8aa614f1a38d491
Violet Replicon <vreplicon@gmail.com>
2020-01-26 10:46:03 -0800

    Added seed files

---
+++ seeds/seed.fabric.sql
@@ -0,0 +1,8 @@
+TRUNCATE fabric RESTART IDENTITY CASCADE;
+
+
+INSERT INTO fabric
+  (quilt_id, fabric_name, amount)
+  VALUES
+    (1, 'Main color', 157),
+    (1, 'Secondary color', 157);
---
+++ seeds/seed.quilts.sql
@@ -0,0 +1,7 @@
+TRUNCATE quilts RESTART IDENTITY CASCADE;
+
+INSERT INTO quilts
+  
+  (quilt_name, width, height, blockWidth, blockHeight, class_name)
+  VALUES
+  ('Stripes', 3, 6, 24, 12, 'striped-block');
